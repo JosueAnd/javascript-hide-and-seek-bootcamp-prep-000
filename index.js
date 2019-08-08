eeb8d189506e6b6fb71def3eb185999bc4e7ae1d
Joshua Andujar <andujar0218@gmail.com>
2019-08-08 00:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
 
+function nestedTarget() {
+    return document.querySelector('*#nested *.target');
+}
+
+function increaseRankBy(n) {
+    const obj = document.querySelectorAll('.ranked-list');
+    for(let index = 0; index < obj.length; index++) {
+        obj[index].innerHTML = (index + n).toString();
+    }
+}
